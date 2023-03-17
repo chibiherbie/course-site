150e0a97a11d1d1192f8746be41079d3e55e6e01
Roman <bekkerrdm@gmail.com>
2023-03-18 02:58:02 +0700

    fix auth api

--- project/auth.py
+++ project/auth.py
@@ -26,13 +26,15 @@ def login_post():
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password):
         flash('Проверь данные и попробуй ещё раз')
+        if api:
+            return {'result': False}
         return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
 
     if api:
-        return {'OK': 'Ok'}
+        return {'result': True}
 
     return redirect(url_for('main.profile'))
 
